4f06a2f59cc0cd8ce97b357a836ed1d6c277d837
LuisMiguelMorenoSivila <morenosivilaluismiguel@gmail.com>
2026-04-25 20:27:28 -0300

    act 1

---
+++ js/ejercicio4.js
@@ -0,0 +1,12 @@
+//Lado
+let ladoA = 8;  
+let ladoB = 5;  
+//area & perimetro
+let area = ladoA * ladoB;                  
+let perimetro = 2 * (ladoA + ladoB);       
+
+//Resultadoss
+console.log("Lado A: " + ladoA  );
+console.log("Lado B: " + ladoB  );
+console.log("Área del rectángulo: " + area + " cm al cuadrado");
+console.log("Perímetro del rectángulo: " + perimetro + " cm");
---
+++ js/ejercicio5.js
@@ -0,0 +1,11 @@
+//Horas & minutos
+let horas = 2;      
+let minutos = 30;   
+
+// Conversión a segundos
+let segundos = (horas * 3600) + (minutos * 60);
+
+// Resultados
+console.log("Horas: " + horas);
+console.log("Minutos: " + minutos);
+console.log("Equivalente en segundos: " + segundos + " s");
