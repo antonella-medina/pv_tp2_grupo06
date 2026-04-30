a27334cc2368b65b3a97ad50ee24cdedb0816da0
Lucas <lucas.tux08@gmail.com>
2026-04-29 22:29:01 -0300

    Lucas: Ejercicio 22 terminado con evento input

---
+++ js/funciones22.js
@@ -0,0 +1,4 @@
+// Esta función toma el valor del input y lo pone en el elemento de destino
+export const actualizarEspejo = (valor, elementoDestino) => {
+    elementoDestino.textContent = valor;
+};
