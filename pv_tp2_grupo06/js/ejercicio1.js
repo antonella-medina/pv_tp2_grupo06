19642ed497f201294f11f0b946220cc63f2477bc
antonella-medina <antonellaxanax@gmail.com>
2026-05-03 17:38:34 -0300

    Corrección de errores en general

--- pv_tp2_grupo06/js/ejercicio1.js
+++
@@ -1,4 +0,0 @@
-const integrantes = "Jose Antonio vilte ,Antonela Medina, Luis Miguel  Moreno Sivila, Lucas Santiago Soto";
-console.log(`Somos ${integrantes} y estamos aprendiendo Javascript`);
- 
- 
